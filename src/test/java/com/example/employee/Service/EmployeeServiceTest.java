653ed9b6124b71e488d5dfbe7f2bf732b0ac477b
Kunal Vora <kunalvora123@gmail.com>
2023-02-26 23:29:18 +0000

    added service testcase

--- src/test/java/com/example/employee/Service/EmployeeServiceTest.java
+++ src/test/java/com/example/employee/Service/EmployeeServiceTest.java
@@ -1,17 +1,24 @@
 package com.example.employee.Service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.employee.Entity.Employee;
 import com.example.employee.Repository.EmployeeRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
     
     @InjectMocks
@@ -21,10 +28,17 @@ public class EmployeeServiceTest {
     private EmployeeRepository empRepo;
 
     @Test
-    void save() {
+    public void save() {
 
         Employee emp1 = new Employee("1","kunaal","kun@g.com", LocalDate.of(2000, Month.APRIL, 12));
 
         when(empRepo.save(any(Employee.class))).thenReturn(emp1);
+        when(empRepo.findAll()).thenReturn(new ArrayList<Employee>( List.of(emp1)) );
+
+
+        List<Employee> listemp = empService.addNewEmployee(emp1);
+
+        assertNotNull(listemp);
+        assertThat(listemp.get(0).getEmpName()).isEqualTo("kunaal");
     }
 }
